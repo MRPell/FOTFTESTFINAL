078847e2ae7e23eb97ed5bf498dc6be6bd0e2994
Matt Pell <MRPell25@gmail.com>
2018-12-16 10:17:54 -0700

    extra comments removed

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -5,13 +5,11 @@ let broadcastListData = [];
 // DOM Ready =============================================================
 $(document).ready(function () {
 
-
+  // Populate the broadcasts on initial page load
   getRecentBroadcasts();
   getFeaturedBroadcasts();
-  // Populate the broadcasts on initial page load
-  //populateRecents();
 
-  //populateFeatured();  
+
 
   //Enable mobile menu Icon to be clicked
   document.getElementById("navIcon").addEventListener("click", toggleFooterMenu);
@@ -67,9 +65,6 @@ let populateFeatured = function (data){
    $('#featuredBroadcastDescription').html('Helping Families Thrive, Hosted by ' + featuredBroadcast.broadcastHosts);
 
 }
-  
-
-
 
 //bring footer menu to front of screen
 function toggleFooterMenu() {
@@ -78,59 +73,4 @@ function toggleFooterMenu() {
   x.toggleClass('bringToFront')
 
 }
-
-// /**
-//  * Fill featured with broadcasts
-//  */
-// function populateFeatured() {
-//   console.log("hi")
-//   $.getJSON('/')
-//     .done(function (data) {
-
-//       let broadcastUrl = data[0].broadcastTitle.replace(/\s/g, '-').toLowerCase();
-//       $('#featuredBroadcastTitle').html(data[0].broadcastTitle);
-//       $('#featuredBroadcastGuests').html(data[0].broadcastGuests);
-//       $('#featuredBroadcastDescription').html('Helping Families Thrive, Hosted by ' + broadcastHosts);
-
-//     })
-//     .fail(function (data) {
-//       console.log("Error retrieving broadcast data");
-//     });
-// }
-
-
-// /**
-//  * Fill recents with broadcasts
-//  */
-// function populateRecents() {
-//   // Empty content string
-//   let listContent = '';
-//   // jQuery AJAX call for JSON
-
-//   $.getJSON('/broadcast/recent')
-//     .done(function (data) {
-
-
-//       // For each item in our JSON,
-//       // add a table row and cells to the content string
-//       $.each(data, function () {
-//         console.log(this.broadcastTitle);
-//         let broadcastUrl = this.broadcastTitle.replace(/\s/g, '-').toLowerCase();
-//         listContent +=
-//           '<a href="/broadcast/details/' + broadcastUrl + '" > <li class="flexContainer recentBroadcastCard"><div><h3>Air Date:</h3>' +
-//           moment(this.broadcastAirDate).format('MM/DD/YYYY') +
-//           '</div><div><h2>' + this.broadcastTitle + '</h2><h3>' + this.broadcastGuests + '</h3></div></li></a > <br /> <hr /> <br />';
-//       });
-//       // Inject the whole content string into our existing HTML table
-//       $('#recentEpisodesList').html(listContent);
-//     })
-
-//     .fail(function (data) {
-//       console.log("Error retrieving broadcast data");
-//     });
-// }
-
-
-
-
 
